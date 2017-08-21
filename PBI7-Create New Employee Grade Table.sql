75d49681b48d2314f95fb6853806619935dad0ed
Daniel Muliawan <danielmuliawann@gmail.com>
2017-08-21 09:26:57 +0800

    Create New Employee Grade Table

--- PBI7-Create New Employee Grade Table.sql
+++ PBI7-Create New Employee Grade Table.sql
@@ -1,7 +1,9 @@
-DROP TABLE EmployeeGrade;
+IF OBJECT_ID('dbo.EmployeeGrade', 'U') IS NOT NULL 
+  DROP TABLE dbo.EmployeeGrade; 
 
 CREATE TABLE EmployeeGrade
-( GradeID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+( 
+  GradeID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
   Name VARCHAR(70) NOT NULL
 );
 
